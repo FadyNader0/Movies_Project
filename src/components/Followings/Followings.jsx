1f12dece43ed79ce38b34d7279a4c29f2f0c5dc3
FadyNader0 <fadyn940@gmail.com>
2025-05-28 08:29:22 +0300

    Update Followings
    update import in this of Followings.css it was import with small letter but it capital letter

--- src/components/Followings/Followings.jsx
+++ src/components/Followings/Followings.jsx
@@ -1,5 +1,5 @@
 import { MdCancel } from "react-icons/md"; 
-import './followings.css'
+import './Followings.css'
 import { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
 import AddStorage from '../Add-Delete-Localsorage/Add-Delete-Localsorage';
