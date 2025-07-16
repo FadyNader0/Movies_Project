a0cb834e022b638bf30bfaa7e56a391a4768f8ca
FadyNader0 <fadyn940@gmail.com>
2025-07-17 02:26:39 +0300

    change in search
    when remove letter from sea5rch bar get back to movie page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -23,7 +23,9 @@ export default function Search() {
     useEffect(() => {
         if (query) {
             navigate(`/movies-page?query=${encodeURIComponent(query)}`);
-        }
+        }else[
+            navigate('/movies-page')
+        ]
     },[query]);
 
     const handleClearHistory = (movie_history_index) => {
